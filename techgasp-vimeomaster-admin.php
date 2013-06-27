dfe84ac53bb6212a99270262869ebe8b7c39c0f6
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-27 18:42:38 +0000

    version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/vimeo-master/trunk@733051 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ techgasp-vimeomaster-admin.php
@@ -0,0 +1,56 @@
+<div class="wrap">
+<div class="icon32" style="width:40px; vertical-align:middle;"><img src="../wp-content/plugins/vimeo-master/techgasp-minilogo.png" alt="' . esc_attr__( 'TechGasp Plugins') . '" /><br /></div>
+
+<h2><b>TechGasp - Vimeo Master Lite Version</b></h2>
+<form method="post" action="techgasp-vimeomaster-admin.php"> 
+<p><b>About TechGasp:</b></p>
+<p>Major player in the Content Management System world! Joomla and Jomsocial Partner with more than 100 high quality, error free Extensions. We provide website customizations and development, SEO Optimization, Facebook Apps, etc. We have fast & furious specialized Hosting for WordPress and Joomla. Our promise, the use of minimal error free code with fast loading times. Also to avoid the implementation of useless Javascript and Ajax in CMS by replacing it with solida php, css, html5 and iframe. Check our website for more high quality extensions. Stay up to date by "like" and "follow" our facebook and twitter page for fresh news, releases and upgrades and updates.</p>
+
+<p>
+<a class="button-primary" href="http://wordpress.techgasp.com" target="_blank" title="Visit Website">Wordpress Website</a> 
+<a class="button-primary" href="http://www.techgasp.com" target="_blank" title="Visit Website">Joomla Website</a>
+<a class="button-primary" href="http://hosting.techgasp.com" target="_blank" title="Visit Website">Hosting Website</a>
+<a class="button-secondary" href="https://www.facebook.com/TechGasp" target="_blank" title="Facebook Page">Facebook Page</a> 
+<a class="button-secondary" href="https://twitter.com/TechGasp" target="_blank" title="Follow Twitter">Twitter Page</a>
+<a class="button-secondary" href="https://plus.google.com/118126459543511361864" target="_blank" title="Follow Google">Google Page</a>
+</p>
+
+<h3>Stay up-to-date with new extension releases, extension updates, & upgrades:</h3>
+<p>
+<span style="float:left; vertical-align:baseline;">
+<fb:like href="https://www.facebook.com/TechGasp" send="true" layout="button_count" width="90" show_faces="false"></fb:like>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=281766848505812";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+&nbsp;
+<a href="https://twitter.com/TechGasp" class="twitter-follow-button" data-show-count="true">Follow @TechGasp</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+</span>
+</p>
+
+<hr>
+
+<img src="../wp-content/plugins/vimeo-master/techgasp-vimeomaster-logo.png" alt="Vimeo Master" align="left" width="387px" height="171px" style="padding:5px;"/>
+<p><h2>Vimeo Master:</h2></p>
+<p>Vimeo Master for let's you integrate the superb Vimeo Video quality into any Wordpress widget position. Only for professional websites.</p>
+</br>
+<p><em>Quality Videos for Wordpress...</em><p>
+</br>
+</br>
+<p><h3>Options & Configuration:</h3></p>
+<p><b>All configuration options are available in your widgets page. Vimeo Master Widget:</b></p>
+<p>Select or <em>"add"</em > Vimeo Master Widget. Configure your settings. Select widget position according to your template. That easy...</p>
+<p><b>Insert Vimeo Master Widget inside Pages and Posts (advanced version only):</b></p>
+<p>Copy and Paste this shortcode inside your pages or posts [vimeomaster id=xx] replace xx with your sidebar number, ie [vimeomaster id=sidebar-1]</p>
+<p><a class="button-primary" href="widgets.php" title="Widgets Page">To Widgets Page</a> <a class="button-secondary" href="http://wordpress.techgasp.com/vimeo-master/" target="_blank" title="More Help">More Help</a> <a class="button-primary" href="http://wordpress.techgasp.com/vimeo-master/" target="_blank" title="Vimeo Master Advanced Version">Vimeo Master Advanced Version</a></p>
+<hr>
+<p><h3>Vimeo Master Lite Version:</h3></p>
+<p>Get the advanced version!!</p>
+<p><a class="button-primary" href="http://wordpress.techgasp.com/vimeo-master/" target="_blank" title="Vimeo Master Advanced Version">Vimeo Master Advanced Version</a></p>
+</form>
+</div>
